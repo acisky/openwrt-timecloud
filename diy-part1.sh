9226784ca7c09fd4503218f8a8af35a3c7d8ea49
acisky <keywing@gmail.com>
2023-01-11 09:35:57 +0800

    disable some

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git cups https://github.com/TheMMcOfficial/lede-cups' >>feeds.conf.default
+# echo 'src-git cups https://github.com/TheMMcOfficial/lede-cups' >>feeds.conf.default
